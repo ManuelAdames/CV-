825782a1555cfcc26c1e0e5cc8cda67c90ea61b8
elgaryy <manuel.adames88@gmail.com>
2021-04-25 20:45:10 -0400

    Fixed router problems, get working

--- router.js
+++ router.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const resumes = require("./resume")
 
-router.get('/resumes', (req, res) =>{
+router.get('/resume', (req, res) =>{
     return res.send(resumes);
 })
+
+module.exports = router;
